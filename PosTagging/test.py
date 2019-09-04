2f4c821e63474d45fc76660eb51e8343ada27252
shihab <shihabhossain611@gmail.com>
2019-09-04 22:05:07 +0600

    Update Commit

---
+++ PosTagging/test.py
@@ -0,0 +1,13 @@
+import csv
+
+def main():
+    # rooted_data = open("reference_root_words.csv", 'r')
+    # with open('suffix.csv', 'w+', newline='') as csvfile:
+    #     suffix_result = csv.writer(csvfile)
+    #     for data in rooted_data:
+    #         suffix_result.writerow([data[0], data[1], data[2]])
+    str = "নিউজসহ"
+
+
+if __name__ == "__main__":
+    main()
